bc9182863f3a8e9b9208a3ea4fe5d84bd7e18ec8
kebeleicsaba <kebeleicsaba@gmail.com>
2020-10-18 11:18:58 +0200

    Még 2 házi megoldva.

--- 004/main.py
+++ 004/main.py
@@ -9,3 +9,24 @@ Kimenet:
 3
 54
 """
+sz1 = int(input())
+sz2 = int(input())
+nagyobb = max([sz1,sz2])
+kisebb = min([sz1,sz2])
+
+# Legnagyobb közös osztó
+legnagyKozOszt = kisebb
+while True:
+    if(kisebb % legnagyKozOszt == 0 and nagyobb % legnagyKozOszt == 0):
+        print(legnagyKozOszt)
+        break
+    legnagyKozOszt -= 1
+
+# Legkisebb közös többszörös
+i = 0
+while True:
+    i += 1
+    legkisKozTobb = nagyobb * i
+    if (legkisKozTobb % kisebb == 0):
+        print(legkisKozTobb)
+        break
--- 005/main.py
+++ 005/main.py
@@ -18,4 +18,27 @@ Pl.:
 48  (66)  -->  12
 49 (100)  -->  1
 """
+a = int(input())
+osszeg = 0
+
+# legnagyobb hatványa hétnek ami kell
+hatvany = 0
+legNagyobbHatvany = 0
+while True:
+    epp = (7 ** hatvany) * 7
+    if epp > a:
+        legNagyobbHatvany = hatvany
+        break
+    hatvany += 1
+
+while legNagyobbHatvany >= 0:
+    n = 7
+    while ((7 ** legNagyobbHatvany) * n) > a:
+        n -= 1
+    osszeg += n
+    a -= (7 ** legNagyobbHatvany) * n
+    legNagyobbHatvany -= 1
+
+print(osszeg)
+
 
