d2477ba9529c34dde781586ef8ae97393ef18bae
kebeleicsaba <kebeleicsaba@gmail.com>
2020-10-24 23:39:12 +0200

    008 javitva

--- 008/main.py
+++ 008/main.py
@@ -2,12 +2,12 @@ x=0.0
 y=0.0
 
 def KetPontTav(x1, y1):
-    return round(((0 - x1) ** 2 + (0 - y1) ** 2) ** 0.5, 2)
+    print(round(((0 - x1) ** 2 + (0 - y1) ** 2) ** 0.5, 0))
 
 while True:
     irany = str(input())
     if irany == "stop": break
-    egyseg = int(input())
+    egyseg = float(input())
     if irany == "forward": y += egyseg
     elif irany == "backward": y -= egyseg
     elif irany == "right": x += egyseg
@@ -15,5 +15,5 @@ while True:
 
 print(round(x,2))
 print(round(y,2))
-print(KetPontTav(x, y))
+KetPontTav(x, y)
     
