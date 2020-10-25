79a28f68c795096231782831a9eb9f0bb7b4e541
kebeleicsaba <kebeleicsaba@gmail.com>
2020-10-25 10:53:59 +0100

    009 megoldva

--- 009/main.py
+++ 009/main.py
@@ -25,9 +25,20 @@ Pelda kimenet:
 Feltetelezhetjuk, hogy legalabb egy nem 0 szamot fogunk kapni.
 
 """
+beker = None
+szamok = []
 
+while beker != 0:
+    beker = int(input())
+    szamok.append(beker)
 
+legnagyobb = 0
+for i in range(len(szamok)):
+    if szamok[i] > legnagyobb: legnagyobb = szamok[i]
 
-
-print(str(     )+": "+str(   ))
+for i in range(1, legnagyobb + 1):
+    db = 0
+    for j in szamok:
+        if i == j: db += 1
+    print(str(i)+": "+str(db))
 
