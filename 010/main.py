6e62f4436c6d96a2b9f98be69a46de060f8bd1e6
kebeleicsaba <kebeleicsaba@gmail.com>
2020-10-26 12:34:16 +0100

    ZH megoldva

--- 010/main.py
+++ 010/main.py
@@ -1,11 +1,27 @@
-
-
-
-
+total = 0
+honap = 12
+percek = []
+smsek = []
 szamla=[]
 
+for i in range(honap):
+    telPerc = int(input())
+    smsDb = int(input())
+    percek.append(telPerc)
+    smsek.append(smsDb)
 
+# Csomag árak
+havidij = int(input())
+percdij = int(input())
+smsdij = int(input())
 
+for i in range(honap):
+    eppen = 0
+    if (percdij * percek[i]) + (smsdij * smsek[i]) > havidij:
+        eppen += (percdij * percek[i]) + (smsdij * smsek[i])
+    else: eppen += havidij
+    szamla.append(eppen)
+    total += eppen
 
 print(szamla)
 print(total)
